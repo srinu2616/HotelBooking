23cc4ea965388d1fd89fd03443ccaa46c41fc5c4
srinu2616 <balayya375@gmail.com>
2025-12-23 21:51:53 +0530

    second

--- backend/configs/db.js
+++ backend/configs/db.js
@@ -2,8 +2,8 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
     try {
-        mongoose.connection.on('connected',()=> console.log("Database Connected"));
-        mongoose.connect(`${process.env.MONGODB_URI}/NexStay`);
+        mongoose.connection.on('connected', () => console.log("Database Connected"));
+        await mongoose.connect(`${process.env.MONGODB_URI}`);
     } catch (error) {
         console.log(error.message);
     }
